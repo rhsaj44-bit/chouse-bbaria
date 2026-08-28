373322f1f3c4255bb0a7f3734c2b2080178f31ad
rhsaj44-bit <rhsaj44@gmail.com>
2026-08-28 13:23:23 -0700

    Create api.js

---
+++ js/api.js
@@ -0,0 +1,17 @@
+// Apps Script-এর Web App URL এখানে বসান
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbynFzoGCFwEU9FbXJcEaLSxiv74VpLOHTsx5JzDPfAqW0iTgTgaXyZVF-VZwwUJw58/exec";
+
+const api = {
+    async request(payload) {
+        try {
+            const response = await fetch(SCRIPT_URL, {
+                method: "POST",
+                body: JSON.stringify(payload)
+            });
+            return await response.json();
+        } catch (error) {
+            console.error("API Error:", error);
+            alert("সার্ভার কানেকশন এরর!");
+        }
+    }
+};
